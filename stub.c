d56d374d77c1fa331bca5d8b68e68452be756746
Bunuel Cubo Soto <bunuel.cubosoto@gmail.com>
2017-10-19 09:43:07 +0200

    Added CeedMath-stub target

---
+++ stub.c
@@ -0,0 +1,18 @@
+#include "TargetConditionals.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+
+#if TARGET_OS_IPHONE // TV, IOS, SIM
+#define CR_HIDDEN_SYMBOL_LINK __private_extern__
+#else // MAC
+#define CR_HIDDEN_SYMBOL_LINK static inline
+#endif
+
+CR_HIDDEN_SYMBOL_LINK int __cr_stub_func() {
+	//#define VARCONCAT(PREFIX, NAME) PREFIX ## NAME
+	//	static int VARCONCAT(__VAR, CREACEED_STUB_FUNCNAME) = 0;
+	return 0;
+}
+
+#pragma clang diagnostic pop
